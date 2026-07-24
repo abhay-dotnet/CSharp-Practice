b3be399cc86709f64a5adcfa574f73a3a5b665b6
abhay-dotnet <abhaysingh26042003@gmail.com>
2026-07-24 19:08:14 +0530

    Add files via upload

---
+++ Programtwo.cs
@@ -0,0 +1,13 @@
+﻿// Write a program to print number 1 to 10 using for loop.
+
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
