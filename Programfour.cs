b31717f5fc4aba59eacfa837017a7c6385683f6b
abhay-dotnet <abhaysingh26042003@gmail.com>
2026-07-28 21:42:31 +0530

    Add files via upload

---
+++ Programfour.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        string s = "babab";
+
+        int[] count = new int[26];
+
+        // Count characters
+        foreach (char c in s)
+        {
+            count[c - 'a']++;
+        }
+
+        // Create left half
+        string left = "";
+
+        for (int i = 0; i < 26; i++)
+        {
+            for (int j = 0; j < count[i] / 2; j++)
+            {
+                left += (char)('a' + i);
+            }
+        }
+
+        // Find middle character
+        char middle = '\0';
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (count[i] % 2 != 0)
+            {
+                middle = (char)('a' + i);
+                break;
+            }
+        }
+
+        // Create right half
+        char[] arr = left.ToCharArray();
+        Array.Reverse(arr);
+
+        string right = new string(arr);
+
+        // Print answer
+        Console.WriteLine("Input: " + s);
+        Console.WriteLine("Output: " + left + middle + right);
+    }
+}
